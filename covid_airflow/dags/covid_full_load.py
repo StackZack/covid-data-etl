2927810d81acb6cecbcbd36dae386cb061380f00
Zackary Luedtke <luedtkezack@gmail.com>
2022-01-02 16:20:19 -0600

    Initial commit of dags. Currently only pulls limit of 1000 records.

---
+++ covid_airflow/dags/covid_full_load.py
@@ -0,0 +1,24 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from socrata_operator import SocrataOperator
+
+
+with DAG(
+    dag_id="covid_full_load",
+    description="Executes ETL for full load of COVID data.",
+    schedule_interval="@daily",
+    start_date=datetime(2022, 1, 1),
+    catchup=False,
+    dagrun_timeout=timedelta(minutes=10),
+    tags=["covid", "etl", "full"]
+) as dag:
+    pull_full_covid_data = SocrataOperator(
+        task_id="pull_full_covid_data",
+        data_id="9bhg-hcku",
+        file_dir="/tmp"
+    )
+
+    pull_full_covid_data
+
+if __name__ == "__main__":
+    dag.cli()
