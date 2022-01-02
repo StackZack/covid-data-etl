fe336b1d71fc684c8a517b573af26b770d38e689
Zackary Luedtke <luedtkezack@gmail.com>
2022-01-02 09:04:29 -0600

    Added logic to Socrata operator for CSV file name.

--- covid_airflow/operators/socrata_operator.py
+++ covid_airflow/operators/socrata_operator.py
@@ -1,18 +1,19 @@
 from airflow.models.baseoperator import BaseOperator
 from hooks.socrata_hook import SocrataHook
+from datetime import datetime
 import pandas as pd
+import os
 
 
 class SocrataOperator(BaseOperator):
-    """Interacts with Socrata to load data and save to CSV file
-    """
+    """Interacts with Socrata to load data and save to CSV file"""
 
     def __init__(self, data_id: str, file_dir: str, **kwargs) -> None:
         """Initializes Socrata operator
 
         :param data_id: unique identifier for Socrata data set
         :type data_id: str
-        :param file_dir: path to save file locally
+        :param file_dir: path to directory to save file
         :type file_dir: str
         """
         self.data_id = data_id
@@ -21,8 +22,16 @@ class SocrataOperator(BaseOperator):
 
     def execute(self):
         """Execute main method for Socrata operator
+
+        :return: file path of saved CSV file with
+        pattern of '<data_id>_<timestamp>'
+        :rtype: str
         """
         socrata_instance = SocrataHook.get_conn()
         response_data = socrata_instance.get(self.data_id)
         df_data = pd.DataFrame.from_records(response_data)
-        df_data.to_csv(self.file_dir)
+        time_stamp = datetime.utcnow().strftime("%Y%m%d%H%M%S")
+        file_name = "_".join(self.data_id, time_stamp) + '.csv'
+        file_path = os.path.join(self.file_dir, file_name)
+        df_data.to_csv(file_path)
+        return file_path
