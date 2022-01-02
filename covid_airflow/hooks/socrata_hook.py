05b34d1222b2ce1cf2ca571cb4f3111f00b750f8
Zackary Luedtke <luedtkezack@gmail.com>
2022-01-02 09:13:54 -0600

    Added Socrata hook init parameter for optional conn_id value.

--- covid_airflow/hooks/socrata_hook.py
+++ covid_airflow/hooks/socrata_hook.py
@@ -9,13 +9,23 @@ class SocrataHook(BaseHook):
 
     SOCRATA_CONN = "http_socrata"
 
+    def __init__(self, *, socrata_conn_id: str = SOCRATA_CONN, **kwargs):
+        """Initializes Socrata hook
+
+        :param socrata_conn_id: Name of Airflow connection to use,
+        defaults to SOCRATA_CONN
+        :type socrata_conn_id: str, optional
+        """
+        self.conn_id = socrata_conn_id
+        super().__init__(**kwargs)
+
     def get_conn(self):
-        """Returns Socrata connection.
+        """Returns Socrata connection
 
         :return: Socrata instance
         :rtype: Socrata
         """
-        conn = self.get_connection(self.SOCRATA_CONN)
+        conn = self.get_connection(self.conn_id)
         return Socrata(
             conn.host,
             conn.extra_dejson["app_token"],
