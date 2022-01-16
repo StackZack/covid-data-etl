8f179673293778a6dfad726933496b8942e1f3c2
Zackary Luedtke <luedtkezack@gmail.com>
2022-01-16 13:44:09 -0600

    Initial commit of SQL schema for data loads.

---
+++ sql/schema.sql
@@ -0,0 +1,20 @@
+CREATE TABLE stg_full_covid (
+	id SERIAL PRIMARY KEY,
+	csv_row BIGINT,
+	data_as_of TIMESTAMP,
+	start_date TIMESTAMP,
+	end_date TIMESTAMP,
+	data_group VARCHAR(8),
+	us_state VARCHAR(13),
+	sex VARCHAR(9),
+	age_group VARCHAR(17),
+	covid_19_deaths BIGINT NULL,
+	total_deaths BIGINT NULL,
+	pneumonia_deaths BIGINT NULL,
+	pneumonia_and_covid_19_deaths BIGINT NULL,
+	influenza_deaths BIGINT NULL,
+	pneumonia_influenza_or_covid BIGINT NULL,
+	footnote TEXT NULL,
+	case_year INT NULL,
+	case_month INT NULL
+);
